447d287527765859eb06c38460ba30978a3cc8fd
Joe Charlier <joe@aepryus.com>
2019-03-15 16:58:58 +0900

    UIColor(rgb:)

--- Acheron/UIColor+Acheron.swift
+++ Acheron/UIColor+Acheron.swift
@@ -16,4 +16,15 @@ extension UIColor {
 	public func tint(_ percent: Float) -> UIColor {
 		return RGB.tint(color: self, percent: percent)
 	}
+	
+	convenience init(red: Int, green: Int, blue: Int) {
+		self.init(red: CGFloat(red)/255, green: CGFloat(green)/255, blue: CGFloat(blue)/255, alpha: 1)
+	}
+	convenience init(rgb: Int) {
+		self.init(
+			red: (rgb >> 16) & 0xFF,
+			green: (rgb >> 8) & 0xFF,
+			blue: rgb & 0xFF
+		)
+	}
 }
