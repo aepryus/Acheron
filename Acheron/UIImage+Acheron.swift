f0332d30009adeb51e57b6bc4459934847b673c4
Joe Charlier <github@aepryus.com>
2019-04-08 06:59:20 +0900

    brokeout async load to UIImage

---
+++ Acheron/UIImage+Acheron.swift
@@ -0,0 +1,44 @@
+//
+//  UIImage+Acheron.swift
+//  Acheron
+//
+//  Created by Joe Charlier on 4/8/19.
+//  Copyright © 2019 Aepryus Software. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+	private static var images: [String:UIImage] = [:]
+	private static var listenerArrays: [String:[(UIImage)->()]] = [:]
+
+	public static func loadImage(url: String, alreadyLoaded: @escaping (UIImage)->(), willLoad: @escaping ()->(), finishedLoading: @escaping (UIImage)->()) {
+		let image: UIImage? = UIImage.images[url]
+		
+		guard image == nil else {alreadyLoaded(image!);return}
+		
+		willLoad()
+		
+		var needsRequest: Bool = false
+		if UIImage.listenerArrays[url] == nil {
+			needsRequest = true
+			UIImage.listenerArrays[url] = []
+		}
+		UIImage.listenerArrays[url]?.append(finishedLoading)
+		
+		guard needsRequest else {return}
+		
+		guard let URL = URL(string: url) else {return}
+		let request = URLRequest(url: URL)
+		NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) { (response: URLResponse?, data: Data?, error: Error?) in
+			guard let data = data else {return}
+			guard let image: UIImage = UIImage(data: data) else {return}
+			UIImage.images[url] = image
+			let listenerArray = UIImage.listenerArrays.removeValue(forKey: url)!
+			listenerArray.forEach {$0(image)}
+		}
+	}
+	public static func loadImage(url: String, _ complete: @escaping (UIImage)->()) {
+		UIImage.loadImage(url: url, alreadyLoaded: complete, willLoad: {}, finishedLoading: complete)
+	}
+}
--- Acheron/UIImageView+Acheron.swift
+++ Acheron/UIImageView+Acheron.swift
@@ -9,8 +9,6 @@
 import UIKit
 
 public extension UIImageView {
-	static var images: [String:UIImage] = [:]
-	static var imageViewSets: [String:Set<UIImageView>] = [:]
 	static var urlLookup: [UInt:String] = [:]
 	
 	var identifier: UInt {
@@ -18,35 +16,19 @@ public extension UIImageView {
 	}
 	
 	func loadImage(url: String, placeholder: UIImage? = nil,_ complete: @escaping ()->()) {
-		self.image = UIImageView.images[url]
-		guard self.image == nil else {return}
-		self.image = placeholder
-
-		let oldURL: String? = UIImageView.urlLookup[identifier]
-		if let oldURL = oldURL, oldURL != url {
-			UIImageView.imageViewSets[oldURL]?.remove(self)
-		}
-		UIImageView.urlLookup[identifier] = url
-
-		var needsRequest: Bool = false
-		if UIImageView.imageViewSets[url] == nil {
-			needsRequest = true
-			UIImageView.imageViewSets[url] = Set<UIImageView>()
-		}
-		UIImageView.imageViewSets[url]!.insert(self)
-		
-		guard needsRequest else {return}
-		
-		guard let URL = URL(string: url) else {return}
-		let request = URLRequest(url: URL)
-		NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) { (response: URLResponse?, data: Data?, error: Error?) in
-			guard let data = data else {return}
-			guard let image: UIImage = UIImage(data: data) else {return}
-			UIImageView.images[url] = image
-			let imageViewSet = UIImageView.imageViewSets.removeValue(forKey: url)!
-			imageViewSet.forEach {$0.image = image}
+		UIImage.loadImage(url: url, alreadyLoaded: { (image: UIImage) in
+			self.image = image
 			complete()
-		}
+			
+		}, willLoad: {
+			self.image = placeholder
+			UIImageView.urlLookup[self.identifier] = url
+
+		}, finishedLoading: { (image: UIImage) in
+			guard UIImageView.urlLookup[self.identifier] == url else {return}
+			self.image = image
+			complete()
+		})
 	}
 	@objc func loadImage(url: String) {
 		loadImage(url: url, placeholder: nil, {})
