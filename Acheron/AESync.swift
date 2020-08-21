f59c55466d4e2ccec2e8be9e9f35b4e994663242
Joe Charlier <joe@aepryus.com>
2020-08-21 10:24:50 +0900

    AESync

---
+++ Acheron/AESync.swift
@@ -0,0 +1,48 @@
+//
+//  AESync.swift
+//  Aexels
+//
+//  Created by Joe Charlier on 5/12/19.
+//  Copyright © 2019 Aepryus Software. All rights reserved.
+//
+
+import Foundation
+import QuartzCore
+
+public class AESync {
+	public var onFire: (CADisplayLink,@escaping ()->())->() = {(link: CADisplayLink,complete: ()->()) in}
+	
+	public lazy var link: CADisplayLink = {
+		let link = CADisplayLink(target: self, selector: #selector(fire))
+		link.preferredFramesPerSecond = 60
+		return link
+	}()
+	
+	public init() {}
+	deinit {
+		link.invalidate()
+	}
+	
+	var running: Bool = false
+	private var semaphore = DispatchSemaphore(value: 1)
+	
+	public func start() {
+		guard !running else { return }
+		running = true
+		link.add(to: .main, forMode: Screen.iPad ? .common : .default)
+	}
+	public func stop() {
+		guard running else { return }
+		running = false
+		semaphore.wait()
+		link.remove(from: .main, forMode: Screen.iPad ? .common : .default)
+		semaphore.signal()
+	}
+	
+	@objc private func fire(link: CADisplayLink) {
+		self.semaphore.wait()
+		onFire(link) {
+			self.semaphore.signal()
+		}
+	}
+}
