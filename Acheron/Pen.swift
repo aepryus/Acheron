a9a63790a5f199056a79c046423d480d17b3e9e6
Joe Charlier <joe@aepryus.com>
2021-03-29 10:42:26 +0900

    improved kern handling

--- Acheron/Pen.swift
+++ Acheron/Pen.swift
@@ -11,26 +11,24 @@ import UIKit
 public class Pen: NSObject {
 	public let attributes: [NSAttributedString.Key:Any]
 	
-	public var font: UIFont { return attributes[NSAttributedString.Key.font] as! UIFont }
-	public var color: UIColor { return attributes[NSAttributedString.Key.foregroundColor] as! UIColor }
-	public var style: NSParagraphStyle { return attributes[NSAttributedString.Key.paragraphStyle] as! NSParagraphStyle }
-	public var alignment: NSTextAlignment { return style.alignment }
+	public var font: UIFont { attributes[NSAttributedString.Key.font] as! UIFont }
+	public var color: UIColor { attributes[NSAttributedString.Key.foregroundColor] as! UIColor }
+	public var style: NSParagraphStyle { attributes[NSAttributedString.Key.paragraphStyle] as! NSParagraphStyle }
+	public var alignment: NSTextAlignment { style.alignment }
+	public var kern: CGFloat? { attributes[NSAttributedString.Key.kern] as? CGFloat }
 	
-	public init(font: UIFont = UIFont.systemFont(ofSize: 16), color: UIColor = UIColor.black, kern: CGFloat? = nil, alignment: NSTextAlignment = .left, style: NSParagraphStyle = NSParagraphStyle.default) {
+	public init(font: UIFont = UIFont.systemFont(ofSize: 16), color: UIColor = UIColor.black, alignment: NSTextAlignment = .left, style: NSParagraphStyle = NSParagraphStyle.default, kern: CGFloat? = nil) {
 		var mutable: [NSAttributedString.Key:Any] = [:]
 		mutable[NSAttributedString.Key.font] = font
 		mutable[NSAttributedString.Key.foregroundColor] = color
-		if let kern = kern { mutable[NSAttributedString.Key.kern] = kern }
 		let mutableStyle: NSMutableParagraphStyle = style.mutableCopy() as! NSMutableParagraphStyle
 		mutableStyle.alignment = alignment
 		mutable[NSAttributedString.Key.paragraphStyle] = mutableStyle
+		if let kern = kern { mutable[NSAttributedString.Key.kern] = kern }
 		attributes = mutable
 	}
-	@objc public convenience init(font: UIFont, alignment: NSTextAlignment) {
-		self.init(font: font, color: UIColor.black, alignment: alignment)
-	}
 
-	public func clone(font: UIFont? = nil, color: UIColor? = nil, kern: CGFloat? = nil, alignment: NSTextAlignment? = nil, style: NSParagraphStyle? = nil) -> Pen {
-		return Pen(font: font ?? self.font, color: color ?? self.color, kern: kern, alignment: alignment ?? self.alignment, style: style ?? self.style)
+	public func clone(font: UIFont? = nil, color: UIColor? = nil, alignment: NSTextAlignment? = nil, style: NSParagraphStyle? = nil, kern: CGFloat? = nil) -> Pen {
+		return Pen(font: font ?? self.font, color: color ?? self.color, alignment: alignment ?? self.alignment, style: style ?? self.style, kern: kern ?? self.kern)
 	}
 }
