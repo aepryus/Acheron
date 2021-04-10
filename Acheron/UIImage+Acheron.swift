8c508759f476ca11883846dbf46dbc012393c29e
Joe Charlier <joe@aepryus.com>
2021-04-10 14:51:57 +0900

    code cleaned

--- Acheron/UIImage+Acheron.swift
+++ Acheron/UIImage+Acheron.swift
@@ -13,29 +13,27 @@ extension UIImage {
 	private static var listeners: [String:[(UIImage)->()]] = [:]
 
 	public static func loadImage(url: String, alreadyLoaded: (UIImage)->(), willLoad: ()->(), finishedLoading: @escaping (UIImage)->()) {
+		guard let URL: URL = URL(string: url) else { return }
+
 		let image: UIImage? = UIImage.images[url]
-		
+
 		guard image == nil else { alreadyLoaded(image!); return }
 		
 		willLoad()
-		
-		var needsRequest: Bool = false
+
 		if UIImage.listeners[url] == nil {
-			needsRequest = true
-			UIImage.listeners[url] = []
-		}
-		UIImage.listeners[url]!.append(finishedLoading)
+			UIImage.listeners[url] = [finishedLoading]
+			URLSession.shared.dataTask(with: URLRequest(url: URL)) { (data: Data?, response: URLResponse?, error: Error?) in
+				guard let data = data, let image: UIImage = UIImage(data: data) else { return }
+				DispatchQueue.main.async {
+					UIImage.images[url] = image
+					let listeners: [(UIImage)->()] = UIImage.listeners.removeValue(forKey: url)!
+					listeners.forEach { $0(image) }
+				}
+			}.resume()
+
+		} else { UIImage.listeners[url]!.append(finishedLoading) }
 		
-		guard needsRequest, let URL = URL(string: url) else { return }
-		
-		URLSession.shared.dataTask(with: URLRequest(url: URL)) { (data: Data?, response: URLResponse?, error: Error?) in
-			guard let data = data, let image: UIImage = UIImage(data: data) else { return }
-			DispatchQueue.main.async {
-				UIImage.images[url] = image
-				let listeners: [(UIImage)->()] = UIImage.listeners.removeValue(forKey: url)!
-				listeners.forEach { $0(image) }
-			}
-		}.resume()
 	}
 	public static func loadImage(url: String, _ complete: @escaping (UIImage)->()) {
 		UIImage.loadImage(url: url, alreadyLoaded: complete, willLoad: {}, finishedLoading: complete)
