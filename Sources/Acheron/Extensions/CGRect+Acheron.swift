7a17a76dc1268f43a481ee29660a28938d70cffa
Joe Charlier <joe.charlier@toptal.com>
2023-09-22 19:48:34 +0900

    CGRect ext

---
+++ Sources/Acheron/Extensions/CGRect+Acheron.swift
@@ -0,0 +1,23 @@
+//
+//  CGRect+Acheron.swift
+//  MacENCx64
+//
+//  Created by Joe Charlier on 2023/06/16.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+extension CGRect {
+    var top: CGFloat { origin.y }
+    var bottom: CGFloat { origin.y + size.height }
+    var left: CGFloat { origin.x }
+    var right: CGFloat { origin.x + size.width }
+    var width: CGFloat { size.width }
+    var height: CGFloat { size.height }
+    
+    public static func * (p: CGRect, q: CGFloat) -> CGRect { CGRect(x: p.left*q, y: p.top*q, width: p.width*q, height: p.height*q) }
+}
+
+#endif
