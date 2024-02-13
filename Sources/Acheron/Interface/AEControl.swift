c4affb404b29b3cb1181f4068de9b62369760584
Joe Charlier <github@aepryus.com>
2024-02-13 11:48:56 +0900

    AEControl

---
+++ Sources/Acheron/Interface/AEControl.swift
@@ -0,0 +1,15 @@
+//
+//  AEControl.swift
+//  Acheron
+//
+//  Created by Joe Charlier on 2/13/24.
+//  Copyright © 2024 Aepryus Software. All rights reserved.
+//
+
+import UIKit
+
+open class AEControl: UIControl {
+    public init() { super.init(frame: .zero) }
+    @available(*, unavailable)
+    required public init?(coder aDecoder: NSCoder) { fatalError() }
+}
